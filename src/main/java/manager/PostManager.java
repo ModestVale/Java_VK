853cf55489e265d78c38b90c27236c79ef859494
Валентина Юрова <visible7@mail.ru>
2021-12-03 17:02:47 +0500

    добавлен PostManager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,18 @@
+package manager;
+
+import domain.VkPost;
+
+public class PostManager {
+    public VkPost[] search(
+            int ownerId,
+            String domain,
+            String query,
+            boolean ownersOnly,
+            int count,
+            int offset) {
+        return null;
+    }
+    
+    public void delete(int ownerId, int postId) {
+    }
+}
